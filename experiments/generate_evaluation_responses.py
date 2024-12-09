c9b351318549e0f99c0addbc22df01f3ec4380d0
Andrew Bell <abell5@users.noreply.github.com>
2024-12-09 15:33:53 -0500

    Add experiment for generating responses

---
+++ experiments/generate_evaluation_responses.py
@@ -0,0 +1,129 @@
+# Base
+import os
+from os.path import join, dirname, pardir
+import pickle
+import argparse
+import time
+
+import sys
+PROJ_PATH = join(dirname(__file__), pardir)
+sys.path.append(PROJ_PATH)
+
+# Core
+from tqdm.auto import tqdm
+import numpy as np
+import pandas as pd
+import torch
+
+# Models / sklearn stuff
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from sklearn.model_selection import GroupKFold
+
+# HuggingFace stuff
+from evaluate import load
+
+# Own
+from mlresearch.model_selection import ModelSearchCV
+from mlresearch.utils import check_pipelines
+
+# Experiments / model wrappers
+DATA_PATH = "data/"
+RESULTS_PATH = "results/"
+from ctg import ModelWrapper
+from ctg.perplexity import PerplexityCustom
+
+#model_paths = ['meta-llama/Meta-Llama-3.1-8B-Instruct',
+#               'Orenguteng/Llama-3.1-8B-Lexi-Uncensored-V2',
+#               'meta-llama/Llama-Guard-3-8B']
+
+def load_evaluation_prompts():
+    df = pd.read_csv(
+        join(DATA_PATH, "evaluation_prompts.csv")
+    )
+    
+    return df
+    
+
+if __name__ == "__main__":
+    parser=argparse.ArgumentParser()
+    parser.add_argument("--model_path", help="model path for experiment", type=str)
+    args=parser.parse_args()
+
+    # Load LLM (llama)
+    cache_dir = "/scratch/alb9742/"
+    model_path = args.model_path
+
+    try:
+        if torch.cuda.memory_allocated(0) == 0:
+            model = AutoModelForCausalLM.from_pretrained(
+                model_path, cache_dir=cache_dir, use_safetensors=True
+            )
+
+            print("DEBUG::GPU memory:: ", torch.cuda.memory_allocated(0))
+            model.cuda()
+            print("DEBUG::Model succesfully moved to Cuda.")
+            print("DEBUG::GPU memory:: ", torch.cuda.memory_allocated(0))
+            CUDA = True
+        else:
+            print("DEBUG::Model already on Cuda.")
+            print("DEBUG::GPU memory:: ", torch.cuda.memory_allocated(0))
+    except:
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path, cache_dir=cache_dir, use_safetensors=True
+        )
+
+        CUDA = False
+
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path, cache_dir=cache_dir, use_safetensors=True
+    )
+    
+    m = ModelWrapper(
+        model, tokenizer, mode="topk", k=100, temperature=1.0, cuda=CUDA
+    )
+
+    eval_df = load_evaluation_prompts()
+    results_df = pd.DataFrame(columns=['dataset','prompt','response','ppl_score','inference_time','num_of_tokens'])
+
+    for i in range(eval_df.shape[0]):
+        # Prompt
+        prompt = eval_df.iloc[i].prompt
+        # Response
+        dataset = eval_df.iloc[i].dataset
+        if dataset == "advbench":
+            target = eval_df.iloc[i].target
+        else:
+            target = ""
+
+        
+        start_time = time.time()
+        # Response
+        response, _ = m.generate(prompt=prompt, target=target, max_tokens=250, verbose=True)
+        end_time = time.time()
+        # Inference time
+        inference_time = end_time - start_time
+
+        # Num of tokens
+        tokens = tokenizer.batch_decode(tokenizer(response)['input_ids'], skip_special_tokens = True, clean_up_tokenization_spaces = True)
+        num_of_tokens = len(tokens)
+
+        # PPL
+        #perplexity = 
+        #print(perplexity)
+        p = PerplexityCustom()
+        score = p.compute(predictions=[prompt], model=model, tokenizer=tokenizer)['perplexities'][0]
+
+        r = {
+            'dataset': dataset,
+            'prompt': prompt,
+            'response': response,
+            'ppl_score': score,
+            'inference_time': inference_time,
+            'num_of_tokens': num_of_tokens
+        }
+
+        results_df.loc[len(results_df)] = r
+
+    filename = f"{RESULTS_PATH}evaluation_responses_{model_path.replace('/', '-')}.pkl"
+    results_df.to_pickle(filename)
